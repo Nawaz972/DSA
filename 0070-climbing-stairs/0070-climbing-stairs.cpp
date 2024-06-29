e50f98de85bbf07dd919eb80bf06ff69243bedca
Nawaz972 <90550141+Nawaz972@users.noreply.github.com>
2024-06-29 18:12:23 +0530

    Time: 3 ms (29.51%), Space: 7.4 MB (11.21%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,29 +1,14 @@
 class Solution {
 public:
-    
-    int myFunc(int n, vector<int> &dp)
-    {
-        if(n == 0)
-        {
-            return 1;
-        }
-        if(dp[n] != -1) return dp[n];
-        int left = 0, right = 0;
-        left = myFunc(n - 1, dp);
-        if(n > 1)   right = myFunc(n - 2, dp);
-        dp[n] = left + right;
-        return dp[n];
-    }
-    
     int climbStairs(int n) {
-        // if(n == 0)  return 1;
-        // int left = 0, right = 0;
-        // left = climbStairs(n - 1);
-        // if(n > 1)   right = climbStairs(n - 2);
-        // return left + right;
-        
+        if(n <= 1)  return 1;
         vector<int> dp(n + 1, -1);
-        int ans = myFunc(n, dp);
-        return ans;
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++)
+        {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n];
     }
 };
